3a3d4ba1d66c2d3572492c6a3b687f0b38997748
gitexproject <gitexproject@git>
2022-09-08 16:39:01 +0530

    modifieds

--- test.php
+++ test.php
@@ -10,6 +10,9 @@ third edit
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 9edc318... modifieds
 test1
 
 test-2
@@ -17,7 +20,10 @@ test-3
 test-4
 =======
 >>>>>>> parent of 5b96a08... test revert
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 7b72299... test-1
 =======
 >>>>>>> parent of 7b72299... test-1
+=======
+>>>>>>> parent of 9edc318... modifieds
